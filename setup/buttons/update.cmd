4b8164ff07e3efdb685d628014676b70b54c70a6
User <joelk05@gmx.de>
2024-05-26 20:34:10 +0200

    Add files via upload

--- setup/buttons/update.cmd
+++ setup/buttons/update.cmd
@@ -8,7 +8,7 @@ for /f "usebackq delims=" %%a in ("%configFile%") do (
     set "installDir=%%a"
 )
 
-echo Please use "Force Stop PM2" before updating WhatsApp-Bot-MD! Do you want to continue? (Y/N)
+echo Please use "Force Stop" before updating BocchiBot! Do you want to continue? (Y/N)
 choice /c yn /n >nul
 
 if errorlevel 2 (
@@ -18,7 +18,7 @@ if errorlevel 2 (
 )
 
 if exist "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" (
-    powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c choco upgrade chocolatey -y && choco upgrade nodejs-lts -y && choco upgrade git -y && choco upgrade ffmpeg -y && choco upgrade curl -y && npm update yarn -y && npm install -g npm@latest && npm install pm2@latest -g && npm install node-gyp -g && pm2 update && cd /d %installDir%\botName && npm update && echo Update complete! && pause'"
+    powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c choco upgrade chocolatey -y && choco upgrade nodejs-lts -y && choco upgrade git -y && choco upgrade ffmpeg -y && npm install npm@latest -g && npm install pm2@latest -g && npm install node-gyp -g && pm2 update && cd /d %installDir%\BocchiBot && npm update && echo Update complete! && pause'"
 ) else (
-    runas /user:Administrator "cmd /c choco upgrade chocolatey -y && choco upgrade nodejs-lts -y && choco upgrade git -y && choco upgrade ffmpeg -y && choco upgrade curl -y && npm update yarn -y && npm install -g npm@latest && npm install pm2@latest -g && npm install node-gyp -g && pm2 update && cd /d %installDir%\botName && npm update && echo Update complete! && pause"
+    runas /user:Administrator "cmd /c choco upgrade chocolatey -y && choco upgrade nodejs-lts -y && choco upgrade git -y && choco upgrade ffmpeg -y && npm install npm@latest -g && npm install pm2@latest -g && npm install node-gyp -g && pm2 update && cd /d %installDir%\BocchiBot && npm update && echo Update complete! && pause"
 )
