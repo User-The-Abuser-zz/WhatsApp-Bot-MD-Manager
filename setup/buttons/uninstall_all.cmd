a15307f4a909dc898d50966cf5912d440adc414e
User <joelk05@gmx.de>
2024-05-27 23:57:40 +0200

    Add files via upload

--- setup/buttons/uninstall_all.cmd
+++ setup/buttons/uninstall_all.cmd
@@ -18,9 +18,55 @@ if errorlevel 2 (
 )
 
 if exist "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" (
-    powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c pm2 kill && echo Uninstalling WhatsApp-Bot-MD. Please wait... && rmdir /s /q %installDir% && choco uninstall ffmpeg -y && choco uninstall curl -y && choco uninstall yarn -y && choco uninstall nodejs-lts -y && choco uninstall git -y && choco uninstall chocolatey -y && rmdir /s /q C:\ProgramData\chocolatey && rmdir /s /q C:\ProgramData\ChocolateyHttpCache && rmdir /s /q C:\Users\%USERNAME%\.chocolatey && del /f C:\Users\%USERNAME%\.yarnrc && rmdir /s /q C:\Users\%USERNAME%\.pm2 && rmdir /s /q C:\Users\%USERNAME%\AppData\Local\npm-cache && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm-cache && rmdir /s /q C:\Program Files\Git && rmdir /s /q C:\Program Files\nodejs && echo Uninstall complete! && pause'"
+    powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c pm2 kill && echo Uninstalling WhatsApp-Bot-MD. Please wait... && rmdir /s /q %installDir% &&
+
+set "packages=ffmpeg nodejs-lts git curl yarn chocolatey"
+
+choco -? >nul 2>&1
+if errorlevel 1 (
+    echo Chocolatey is not installed.
+    exit /b 1
+)
+
+for %%i in (%packages%) do (
+    choco list --local-only | findstr /c:"%%i" >nul
+    if not errorlevel 1 (
+        echo Uninstalling %%i...
+        choco uninstall %%i -y
+    ) else (
+        echo %%i is not installed.
+    )
+)
+
+exit /b
+
+
+&& rmdir /s /q C:\ProgramData\chocolatey && rmdir /s /q C:\ProgramData\ChocolateyHttpCache && rmdir /s /q C:\Users\%USERNAME%\.chocolatey && del /f C:\Users\%USERNAME%\.yarnrc && rmdir /s /q C:\Users\%USERNAME%\.pm2 && rmdir /s /q C:\Users\%USERNAME%\AppData\Local\npm-cache && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm-cache && rmdir /s /q C:\Program Files\Git && rmdir /s /q C:\Program Files\nodejs && echo Uninstall complete! && pause'"
 ) else (
-    runas /user:Administrator "cmd /c pm2 kill && echo Uninstalling WhatsApp-Bot-MD. Please wait... && rmdir /s /q %installDir% && choco uninstall ffmpeg -y && choco uninstall curl -y && choco uninstall yarn -y && choco uninstall nodejs-lts -y && choco uninstall git -y && choco uninstall chocolatey -y && rmdir /s /q C:\ProgramData\chocolatey && rmdir /s /q C:\ProgramData\ChocolateyHttpCache && rmdir /s /q C:\Users\%USERNAME%\.chocolatey && del /f C:\Users\%USERNAME%\.yarnrc && rmdir /s /q C:\Users\%USERNAME%\.pm2 && rmdir /s /q C:\Users\%USERNAME%\AppData\Local\npm-cache && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm-cache && rmdir /s /q C:\Program Files\Git && rmdir /s /q C:\Program Files\nodejs && echo Uninstall complete! && pause"
+    runas /user:Administrator "cmd /c pm2 kill && echo Uninstalling WhatsApp-Bot-MD. Please wait... && rmdir /s /q %installDir% &&
+
+set "packages=ffmpeg nodejs-lts git curl yarn chocolatey"
+
+choco -? >nul 2>&1
+if errorlevel 1 (
+    echo Chocolatey is not installed.
+    exit /b 1
+)
+
+for %%i in (%packages%) do (
+    choco list --local-only | findstr /c:"%%i" >nul
+    if not errorlevel 1 (
+        echo Uninstalling %%i...
+        choco uninstall %%i -y
+    ) else (
+        echo %%i is not installed.
+    )
+)
+
+exit /b
+
+
+&& rmdir /s /q C:\ProgramData\chocolatey && rmdir /s /q C:\ProgramData\ChocolateyHttpCache && rmdir /s /q C:\Users\%USERNAME%\.chocolatey && del /f C:\Users\%USERNAME%\.yarnrc && rmdir /s /q C:\Users\%USERNAME%\.pm2 && rmdir /s /q C:\Users\%USERNAME%\AppData\Local\npm-cache && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm-cache && rmdir /s /q C:\Program Files\Git && rmdir /s /q C:\Program Files\nodejs && echo Uninstall complete! && pause"
 )
 
 if exist "%USERPROFILE%\Desktop\WhatsApp-Bot-MD Manager.lnk" (
