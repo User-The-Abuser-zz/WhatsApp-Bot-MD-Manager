40715ba2ab00ea10b24e0f33200fd06d3c400ed9
User <joelk05@gmx.de>
2024-05-27 17:23:25 +0200

    Add files via upload

--- setup/buttons/uninstall_all.cmd
+++ setup/buttons/uninstall_all.cmd
@@ -17,32 +17,6 @@ if errorlevel 2 (
     exit /b
 )
 
-echo Uninstalling Node.js. Please wait...
-
-choco uninstall nodejs -y
-
-:check_node
-where node >nul 2>nul
-if %errorlevel% neq 0 (
-    timeout /t 1 /nobreak >nul
-    goto check_node
-)
-
-echo Node.js successfully uninstalled!
-
-echo Uninstalling Git. Please wait...
-
-choco uninstall git -y
-
-:check_git
-where git >nul 2>nul
-if %errorlevel% neq 0 (
-    timeout /t 1 /nobreak >nul
-    goto check_git
-)
-
-echo Git successfully uninstalled!
-
 if exist "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" (
     powershell -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c pm2 kill && echo Uninstalling WhatsApp-Bot-MD. Please wait... && rmdir /s /q %installDir% && choco uninstall ffmpeg -y && choco uninstall curl -y && choco uninstall yarn -y && choco uninstall nodejs-lts && choco uninstall git && choco uninstall chocolatey -y && rmdir /s /q C:\ProgramData\chocolatey && rmdir /s /q C:\ProgramData\ChocolateyHttpCache && rmdir /s /q C:\Users\%USERNAME%\.chocolatey && del /f C:\Users\%USERNAME%\.yarnrc && rmdir /s /q C:\Users\%USERNAME%\.pm2 && rmdir /s /q C:\Users\%USERNAME%\AppData\Local\npm-cache && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm && rmdir /s /q C:\Users\%USERNAME%\AppData\Roaming\npm-cache && rmdir /s /q C:\Program Files\Git && rmdir /s /q C:\Program Files\nodejs && echo Uninstall complete! && pause'"
 ) else (
